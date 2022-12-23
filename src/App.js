305b47a422196ce7c07b1ee960d1981a7c07f03b
Vanilka18 <120592824+Vanilka18@users.noreply.github.com>
2022-12-23 10:13:27 +0100

    jula

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 const ola = "ola barańska"
 
 function App() {
+  const jula = "Jula Dobrzyńska-Kobylec"
   return (
     <div className="App">
       <div>{ola}</div>
-  <div>Jula Dobrzyńska-Kobylec</div>
+  <div>{jula}</div>
   <div>Violetta Wolniewicz</div>
     </div>
   );
