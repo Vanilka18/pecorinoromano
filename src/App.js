5349e5206977dd815194b342c565294754c12e8d
bartekbf10 <bartekbf10@gmail.com>
2022-12-29 22:55:51 +0100

    komentarz

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
-  const jula = "Jula Dobrzyńska-Kobylec"
-  const viola = "Violetta Wolniewicz"
+  const jula = "Jula Dobrzyńska-Kobylec";
+  const viola = "Violetta Wolniewicz";
+  const bartek = "Bartosz Frankowski";
   return (
     <div className="App">
       <div>{ola}</div>
-  <div>{jula}</div>
-  <div>{viola}</div>
-  
+      <div>{jula}</div>
+      <div>{viola}</div>
+      <div>{bartek}</div>
     </div>
   );
 }
