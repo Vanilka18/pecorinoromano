78728051b0e0057fc33abdfaaf72aa201ba769b6
Vanilka18 <120592824+Vanilka18@users.noreply.github.com>
2023-02-03 17:36:57 +0100

    style

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,16 @@
+import styled from "styled-components";
 import { Badges } from "./Badges";
 import { Name } from "./Name";
 
 export const Profile = ({user}) => {
   
-    return  <div><Name name={user.name} />
-    <Badges badges={user.badges} /></div>;
+    return  <StyledWrapper><Name name={user.name} />
+    <Badges badges={user.badges} /></StyledWrapper>;
   };
+  const StyledWrapper= styled ("div") (()=>({
+    border: "1px solid black", 
+    width: "250px",
+    padding: "10px",
+    margin: "10px"
+
+  }))
